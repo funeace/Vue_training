e52d8c4c7f231af798b01bb027bbca7cf099e019
Kengo <funeace@gmail.com>
2020-05-24 21:21:49 +0900

    [Add]hello_app

---
+++ chapter3/hello_app/src/App.vue
@@ -0,0 +1,40 @@
+<template>
+  <div id="app">
+    <HelloWorld v-bind:title="message" v-on:result-event="appAction" />
+    <hr>
+    <p>{{ result }}</p>
+  </div>
+</template>
+
+<script>
+import HelloWorld from './components/HelloWorld.vue'
+
+export default {
+  name: 'app',
+  components: {
+    HelloWorld
+  },
+  data: function(){
+    return{
+      message: "Hello",
+      result: "no event"
+    };
+  },
+  methods:{
+    appAction:function(message) {
+      this.result = '(*** you send:"' + message + '". ***)';
+    }
+  }
+}
+</script>
+
+<style>
+#app {
+  font-family: Avenir, Helvetica, Arial, sans-serif;
+  -webkit-font-smoothing: antialiased;
+  -moz-osx-font-smoothing: grayscale;
+  text-align: center;
+  color: #2c3e50;
+  margin-top: 60px;
+}
+</style>
