45cceb3a5acbbf2c8583e062916939dfede6faf0
Kengo <funeace@gmail.com>
2020-05-25 01:27:09 +0900

    [Add]4-1

---
+++ chapter4/hello_app/src/App.vue
@@ -0,0 +1,33 @@
+<template>
+  <div id="app">
+    <HelloWorld v-bind:title="message" v-bind:num="num" />
+  </div>
+</template>
+
+<script>
+import HelloWorld from './components/HelloWorld.vue'
+
+export default {
+  name: 'app',
+  components: {
+    HelloWorld
+  },
+  data: function(){
+    return{
+      message: "Validdate",
+      num: "abc"
+    };
+  }
+}
+</script>
+
+<style>
+#app {
+  font-family: Avenir, Helvetica, Arial, sans-serif;
+  -webkit-font-smoothing: antialiased;
+  -moz-osx-font-smoothing: grayscale;
+  text-align: center;
+  color: #2c3e50;
+  margin-top: 60px;
+}
+</style>
