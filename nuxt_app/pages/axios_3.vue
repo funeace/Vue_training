adfa04ea68df459c731cdb44ecb7094f7c7b37db
Kengo <funeace@gmail.com>
2020-05-26 19:03:46 +0900

    [Add]6-2

---
+++ nuxt_app/pages/axios_3.vue
@@ -0,0 +1,75 @@
+<!-- axiosで指定先のデータ全てを持ってくる -->
+<template>
+  <section class="container">
+    <h1>{{title}}</h1>
+    <p>{{message}}</p>
+    <ul v-for="(data, key) in json_data">
+      <li>{{data.name}} ({{data.age}}) [{{key}}]</li>
+    </ul>
+  </section>
+</template>
+
+<script>
+  const axios = require('axios')
+  let url = "https://funeace-sample-vue.firebaseio.com/person.json"
+
+  export default{
+    data: function(){
+      return {
+        title: "Axios",
+        message: "axios sample"
+      }
+    },
+    // 非同期の処理
+    asyncData: async function(){
+      let result = await axios.get(url)
+      return{ json_data: result.data }
+    },
+  }
+</script>
+<style>
+  ul {
+    margin: 0px 10px;
+    background-color: aliceblue;
+  }
+  li {
+    padding: 10px;
+    font-size: 16pt;
+  }
+  tr th {
+    width:150px;
+    background-color: darkblue;
+    color:white;
+    font-size:16pt;
+  }
+  tr td {
+      padding:5px 10px;
+      background-color:#eef;
+      font-size:14pt;
+  }
+
+  .container {
+    padding: 5px 10px;
+  }
+  h1 {
+    font-size: 60pt;
+    color: #345980;
+  }
+  p {
+    padding-top:5px;
+    font-size: 20pt;
+  }
+  div {
+      font-size:14pt;
+  }
+  pre {
+    padding: 10px;
+    font-size: 18pt;
+    background-color: #efefef;
+    white-space: pre-wrap ;
+  }
+  hr {
+    margin:10px 0px;
+  }
+
+</style>
